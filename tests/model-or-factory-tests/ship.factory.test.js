54c6f6d1a5192b7fb059c77ce06ff5da6ea6511f
Edward Iga Kigongo <igakigongo@gmail.com>
2020-02-18 12:59:47 +0100

    separate factory and api/behavioral tests

--- tests/model-or-factory-tests/ship.factory.test.js
+++ tests/model-or-factory-tests/ship.factory.test.js
@@ -1,6 +1,5 @@
 import Ship from '../../src/models/ship.factory';
 
-
 describe('ship factory', () => {
   it('throws an error if positions is not an array', () => {
     expect(() => new Ship()).toThrow();
@@ -17,30 +16,5 @@ describe('ship factory', () => {
 
   it('creates a valid ship if all positions are numbers', () => {
     expect(() => new Ship([1, 2, 3, 4])).not.toThrow();
-  });
-});
-
-
-describe('ship api', () => {
-  it('ship.hit() throws error if a position is not a number', () => {
-    const ship = new Ship([1, 2, 4, 5]);
-    expect(() => ship.hit()).toThrow();
-  });
-
-  it('ship.hit() throws error if a position is not a valid ship position', () => {
-    const ship = new Ship([1, 2, 4, 5]);
-    expect(() => ship.hit(10)).toThrow();
-  });
-
-  it('ship.hit(5) marks a position as hit', () => {
-    const ship = new Ship([1, 2, 4, 5]);
-    expect(() => ship.hit(4)).not.toThrow();
-  });
-
-  it('is sunk when all positions are hit', () => {
-    const positions = [1, 2, 4, 5];
-    const ship = new Ship(positions);
-    positions.forEach(p => ship.hit(p));
-    expect(ship.isSunk).toBe(true);
   });
 });
