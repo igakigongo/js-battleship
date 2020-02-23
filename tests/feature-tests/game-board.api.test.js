3a580202cfadc1a57b478f72121dae04d3aa2d93
Edward Iga Kigongo <igakigongo@gmail.com>
2020-02-23 15:58:09 +0100

    refactor tests - reduce data redundancy in tests

--- tests/feature-tests/game-board.api.test.js
+++ tests/feature-tests/game-board.api.test.js
@@ -2,6 +2,10 @@ import Coordinate from '../../src/js/models/coordinate';
 import GameBoard from '../../src/js/models/game-board.factory';
 
 describe('game board api', () => {
+  const [coords1, coords2] = [
+    [new Coordinate(0, 0), new Coordinate(0, 1)],
+    [new Coordinate(5, 5), new Coordinate(5, 6)]];
+
   let board;
   beforeEach(() => {
     board = new GameBoard();
@@ -13,16 +17,17 @@ describe('game board api', () => {
   });
 
   it('should not place a ship at a taken position', () => {
-    board.placeShipAt([new Coordinate(0, 0), new Coordinate(0, 1)]);
-    board.placeShipAt([new Coordinate(5, 5), new Coordinate(5, 6)]);
+    board.placeShipAt(coords1);
+    board.placeShipAt(coords2);
+    const occupiedCoordinate = new Coordinate(5, 5);
     expect(board.ships.length).toEqual(2);
-    expect(() => board.placeShipAt([new Coordinate(5, 5)])).toThrow('Position (5, 5) is already occupied by a ship');
+    expect(() => board.placeShipAt([occupiedCoordinate])).toThrow('Position (5, 5) is already occupied by a ship');
     expect(board.ships.length).toEqual(2);
   });
 
   it('should hit a ship at the specified position', () => {
-    board.placeShipAt([new Coordinate(0, 0), new Coordinate(0, 1)]);
-    board.placeShipAt([new Coordinate(5, 5), new Coordinate(5, 6)]);
+    board.placeShipAt(coords1);
+    board.placeShipAt(coords2);
     const targetCoordinate = new Coordinate(5, 6);
     board.receiveAttack(targetCoordinate);
     const { x, y } = targetCoordinate;
@@ -32,7 +37,7 @@ describe('game board api', () => {
   });
 
   it('should record a missed attack', () => {
-    board.placeShipAt([new Coordinate(0, 0), new Coordinate(0, 1)]);
+    board.placeShipAt(coords1);
     const targetCoordinate = new Coordinate(5, 6);
     const missedAttacks = board.missedAttacks.length;
     board.receiveAttack(targetCoordinate);
@@ -40,16 +45,12 @@ describe('game board api', () => {
   });
 
   it('should report true when all ships are sunk', () => {
-    const coords1 = [new Coordinate(0, 0), new Coordinate(0, 1)];
-    const coords2 = [new Coordinate(5, 5), new Coordinate(5, 6)];
     [coords1, coords2].forEach(coords => { board.placeShipAt(coords); });
     [...coords1, ...coords2].forEach(c => { board.receiveAttack(c); });
     expect(board.allShipsSunk).toBe(true);
   });
 
   it('should report false when some ships are not sunk', () => {
-    const coords1 = [new Coordinate(0, 0), new Coordinate(0, 1)];
-    const coords2 = [new Coordinate(5, 5), new Coordinate(5, 6)];
     [coords1, coords2].forEach(coords => { board.placeShipAt(coords); });
     [...coords2].forEach(c => { board.receiveAttack(c); });
     expect(board.allShipsSunk).toBe(false);
